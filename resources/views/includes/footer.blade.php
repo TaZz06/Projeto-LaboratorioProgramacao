a4599ff3db744d7274fdd1e169a843addd0d3d2c
RicardoPinheiro <40108@ufp.edu.pt>
2021-12-28 03:27:59 +0000

    Revert "Projeto inical"
    This reverts commit adf39669103ebc8ecfbe1f5c96866eae83bddf78.

--- resources/views/includes/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="page-footer font-small blue">
-    <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        Programming Lab. Lectures
-    </div>
-</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PhotoController;
-use App\Http\Controllers\PDFController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,15 +16,3 @@ use App\Http\Controllers\PDFController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Auth::routes();
-Auth::routes(['verify' => true]);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
-
-
-Route::post('save', [PhotoController::class, 'store'])->name('upload.picture')->middleware('is_admin');
-
-
-Route::get('generate-pdf', [PDFController::class, 'generatePDF']);
